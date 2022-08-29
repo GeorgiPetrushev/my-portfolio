c9f551825ba89f7f68befd3d4ef50bfb3e1223d5
Georgi <figoto660@gmail.com>
2022-08-28 22:02:12 -0400

    add footer fix phone screen menu bug

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header.js";
 import Home from "./components/Home.js";
 import Projects from "./components/Projects.js";
 import ContactMe from "./components/ContactMe.js";
+import Footer from "./components/Footer.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,10 +10,11 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-       <Route path="/" element={<Home />} />
-       <Route path="/projects" element={<Projects />} />
-       <Route path="/contact" element={<ContactMe />} />z
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<ContactMe />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
