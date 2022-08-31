5bf326d49f5fdcf86ecd93b75112a4ae3019e08c
Georgi <figoto660@gmail.com>
2022-08-30 22:11:28 -0400

    add skills page

--- src/components/styles/Header.style.js
+++ src/components/styles/Header.style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const HeaderStyle = styled.div`
   background-color: black;
   width: 100%;
-  height: 10vh;
+  height: 12vh;
   padding: 0 1.5rem;
   @media screen and (min-width: 768px) {
     padding: 0 1.5rem;
