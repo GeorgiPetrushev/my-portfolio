a1a0a7efb547558752328972020be8bdce967f92
Georgi <figoto660@gmail.com>
2022-08-31 23:38:11 -0400

    Home page style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { HomeStyle } from "./styles/Home.style";
+import profilePicture from "../pictures/profile-pic.jpg";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <HomeStyle className="container">
+      <div className="left-side">
+        <img src={profilePicture} alt="Logo" />
+        <h2>Georgi Petrushev</h2>
+        <h3>Front-End Development</h3>
+      </div>
+      <div className="right-side">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo atque
+          iste iure ullam nihil nulla amet earum cupiditate fugit omnis nemo,
+          repellat facilis voluptatum natus labore accusamus quidem cum
+          doloribus!
+        </p>
+      </div>
+    </HomeStyle>
+  );
 };
 
 export default Home;
---
+++ src/components/styles/Home.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HomeStyle = styled.div`
+  margin: 3rem auto;
+  min-height: 80vh;
+  background-color: green;
+
+  img {
+    border-radius: 3rem;
+  }
+`;
