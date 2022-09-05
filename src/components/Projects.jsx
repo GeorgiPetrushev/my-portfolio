80bae384006e13f45d27c0e2f82dd1ca3086d0ba
Georgi <figoto660@gmail.com>
2022-09-05 15:16:38 -0700

    creating <ls> arr object

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import { ProjectsStyle } from "./styles/Projects.style";
+import { projectsArr } from "./projectObjects";
 
 const Projects = () => {
-  
+  const listItems = projectsArr.map((number) => (
+    <li key={number.key} className="project">
+      {number.tittle}
+    </li>
+  ));
+
   return (
     <ProjectsStyle>
       <div className="project-tittle">Projects</div>
-      <ul className="container-projects">
-        <li className="project">Project 1</li>
-        <li className="project">Project 1</li>
-        <li className="project">Project 1</li>
-        <li className="project">Project 1</li>
-        <li className="project">Project 1</li>
-        <li className="project">Project 1</li>
-        <li className="project">Project 1</li>
-        <li className="project">Project 1</li>
-        <li className="project">Project 1</li>
-      </ul>
+      <ul className="container-projects">{listItems}</ul>
     </ProjectsStyle>
   );
 };
