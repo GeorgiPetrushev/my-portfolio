936d36bee8db0b01774f78d00ae6faf63a0c12fa
Georgi <figoto660@gmail.com>
2022-10-06 14:26:58 -0400

    save

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,25 +13,21 @@ const Home = () => {
           <h2>Georgi Petrushev</h2>
           <h3>Web Developer</h3>
         </div>
-        <div className="right-side">
-          <p>
-            I graduated technical university of Sofia major telecommunication.
-            But I decided to continue in the software developer industry in
-            2019, which I love very much. I am a highly motivated, dedicated and
-            ambitious software developer who is ready to fulfill any task
-            assigned to me. Thanks to my background, I gained problem-solving
-            skills. I am very interested in new technologies and I am trying to
-            grasp it. I am want to be a web developer and am constantly
-            researching and improving myself. I believe in lifelong learning.I'm
-            abel really quick to learn new languages and libraries if is
-            necessary.{" "}
-          </p>
-          <p>
-            I'm currently doing front development (Java Script | React |) 👨🏼‍🤝‍👨🏻 I
-            want to collaborate on open source projects 🥅 Goals: Develop
-            application projects useful to humanity and to participate in these
-            projects in the world. 💬 You can ask me anything you want to know
-          </p>
+        <div className="right-side"><p>
+          Dedicated individual with a passion to code for Web Development.
+          Typically, code as a hobby, through self-developed projects and
+          programs. Previously and actively progressed my programming skills
+          with different online courses, such as TeamTreeHouse, FreeCodeCamp and
+          Udemy. Understanding that coding is an infinite cycle constantly
+          evolving as technology progresses. I look forward to supporting my
+          company to code with innovation. In each course, we learn to develope
+          different sets of technical skills, such as HTML, CSS, JavaScript,
+          jQuery, Bootstrap, etc. </p><p>As
+          a professional and a person with much humility I’m aware that
+          technology is constantly evolving so I am proactive in educating
+          myself as much as possible to enhance my programming skills,
+          knowledge, and portfolio. Lastly, I commend myself on being a prompt
+          learner with a passion to problem-solve regardless of any issue.</p>
         </div>
       </HomeStyle>
       <Skills />
